10b40676b810a96abfaccce9804c0cf12e87d6c8
Jordi Galaso Serret <jordigalasoserret@gmail.com>
2023-09-09 19:12:57 +0200

    [DOC] Added minor documentation

--- JG_API_papernest.py
+++ JG_API_papernest.py
@@ -21,16 +21,19 @@ def get_towers_coverage(address=None):
     In the case of an error: (str) error description
     In any other case: (dict) Operators and their coverage
     """
+    # Let the Locator handle the location
     try:
         location = Locator(address)
     except AttributeError as error:
         return error.args[0]
 
+    # Let the TowerManager handle the context for the location
     try:
         tower_mgr = TowerManager(location)
     except AttributeError as error:
         return error.args[0]
 
+    # Let the TowerManager get the coverage for the location
     tower_mgr.location_coverage()
 
     return tower_mgr.towers_coverage
